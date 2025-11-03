dd7b8b337716359403badadcff28104def13b4d6
Yander Sanchez <sanchezyander@gmail.com>
2025-11-03 17:33:52 -0400

    feat: add vuetify

--- src/main.ts
+++ src/main.ts
@@ -5,9 +5,20 @@ import AppRouter from '@/router';
 import AppI18n from './locales';
 import AppStore from './stores';
 
+// Vuetify
+import 'vuetify/styles';
+import { createVuetify } from 'vuetify';
+import * as components from 'vuetify/components';
+import * as directives from 'vuetify/directives';
+
 import { validateEnvironmentVariables } from '../scripts/env';
 
 const app = createApp(App);
+
+const vuetify = createVuetify({
+  components,
+  directives,
+});
 
 if (import.meta.env.VITE_ENV == 'development') {
   validateEnvironmentVariables();
@@ -17,5 +28,6 @@ if (import.meta.env.VITE_ENV == 'development') {
 app.use(AppI18n);
 app.use(AppRouter);
 app.use(AppStore);
+app.use(vuetify);
 
 app.mount('#app');

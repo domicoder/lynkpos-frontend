c63939232baf64cdae745aa1f4577544053c509a
Yander Sanchez <sanchezyander@gmail.com>
2025-11-04 18:20:43 -0400

    chore: temporary unused var in auth.ts for husky

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -1,20 +1,20 @@
-import { Role as R } from '@/domains/Role';
-import type { Role as UserRole } from '@/domains/User';
+// import { Role as R } from '@/domains/Role';
+// import type { Role as UserRole } from '@/domains/User';
 import { LOGIN_VIEW, PAGE_NOT_FOUND } from '@/router/paths';
 import useAuthStore from '@/stores/user/AuthStore';
 import type { NavigationGuardNext, RouteLocationNormalized } from 'vue-router';
 
 // TODO: handle required roles
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-const hasRequiredRoles = (userRoles: UserRole[], accessRoles: R[]) => {
-  const draft = userRoles.map((g) => g.role_name);
+// const hasRequiredRoles = (userRoles: UserRole[], accessRoles: R[]) => {
+//   const draft = userRoles.map((g) => g.role_name);
 
-  if (accessRoles.includes(R.Admin) && accessRoles.includes(R.User)) {
-    return draft.includes(R.Admin) || draft.includes(R.User);
-  }
+//   if (accessRoles.includes(R.Admin) && accessRoles.includes(R.User)) {
+//     return draft.includes(R.Admin) || draft.includes(R.User);
+//   }
 
-  return accessRoles.every((role) => draft.includes(role));
-};
+//   return accessRoles.every((role) => draft.includes(role));
+// };
 
 export function requireAuth(
   to: RouteLocationNormalized,

a72e48ad7b0ecdf2dc6e92681d5e3926ad7eff1b
Yander Sanchez <sanchezyander@gmail.com>
2025-11-18 19:46:22 -0400

    chore(core): update bypass GitGuardian

--- src/views/login/LoginView.vue
+++ src/views/login/LoginView.vue
@@ -23,8 +23,7 @@
   const handleLogin = async () => {
     try {
       const token = {
-        token:
-          'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1laWRlbnRpZmllciI6IjRjMjI1MThjLTAwMDktNDA1MC1lMmM5LTA4ZGUxZTMyYThmMSIsImh0dHA6Ly9zY2hlbWFzLm1pY3Jvc29mdC5jb20vd3MvMjAwOC8wNi9pZGVudGl0eS9jbGFpbXMvcm9sZSI6IjEiLCJleHAiOjE3NjM5OTI1NjQsImlzcyI6InB1bnRvX3ZlbnRhIiwiYXVkIjoidXNlciJ9.A7nrbaLWE64n3cYr2IhEUCjMTaFVTP0A4oI6_mZNd5E',
+        token: 'fake_token',
       };
       const userInfo = {
         id: '4c22518c-0009-4050-e2c9-08de1e32a8f1',
